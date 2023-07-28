c2a00fb4dc824d34c74b54b96ea29221f8bd8a90
Konstantin Lindorfer <konstantin.lindorfer@spyteam.one>
2023-07-28 22:24:50 +0200

    Improved thread-safety of Skyblock presence checking, fixed not finding Skyblock when animated, moved event firing to property

--- src/main/kotlin/com/github/spyunderscore04/omegaskyblock/Player.kt
+++ src/main/kotlin/com/github/spyunderscore04/omegaskyblock/Player.kt
@@ -2,14 +2,26 @@ package com.github.spyunderscore04.omegaskyblock
 
 import com.github.spyunderscore04.omegaskyblock.events.SkyblockJoinEvent
 import com.github.spyunderscore04.omegaskyblock.events.SkyblockLeaveEvent
+import com.github.spyunderscore04.omegaskyblock.utils.unformatted
 import net.minecraft.client.Minecraft
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.event.world.WorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.network.FMLNetworkEvent
+import java.util.concurrent.atomic.AtomicBoolean
 
 object Player {
     private var playsSkyblock = false
-    private var skyblockCheckInProgress = false
+        set(value) {
+            // fire events if necessary
+            when {
+                !field && value -> MinecraftForge.EVENT_BUS.post(SkyblockJoinEvent())
+                field && !value -> MinecraftForge.EVENT_BUS.post(SkyblockLeaveEvent())
+            }
+            field = value
+        }
+
+    private var skyblockCheckInProgress = AtomicBoolean(false)
 
     @SubscribeEvent
     fun onWorldLoad(event: WorldEvent.Load) {
@@ -17,27 +29,33 @@ object Player {
         Thread { checkPlaysSkyblock() }.start()
     }
 
+    @SubscribeEvent
+    fun onLeaveServer(event: FMLNetworkEvent.ClientDisconnectionFromServerEvent) {
+        // definitely leave Skyblock
+        playsSkyblock = false
+    }
+
     private fun checkPlaysSkyblock() {
         // only one check running at a time
-        if (skyblockCheckInProgress) return
-        skyblockCheckInProgress = true
+        if (!skyblockCheckInProgress.compareAndSet(false, true)) return
 
-        val playedSkyblock = playsSkyblock
+        try {
+            // await info
+            var checked = false
+            while (!checked) Minecraft.getMinecraft().theWorld
+                ?.scoreboard
+                ?.getObjectiveInDisplaySlot(1)
+                ?.displayName
+                ?.let {
+                    playsSkyblock = it
+                        .unformatted()
+                        .contains("skyblock", true)
+                    checked = true
+                }
+                ?: Thread.sleep(100)
 
-        // await info
-        while (skyblockCheckInProgress) Minecraft.getMinecraft().theWorld
-            ?.scoreboard
-            ?.getObjectiveInDisplaySlot(1)
-            ?.displayName?.let {
-                // perform check
-                playsSkyblock = it.contains("skyblock", true)
-                skyblockCheckInProgress = false
-            }
-            ?: Thread.sleep(100)
-
-        when {
-            !playedSkyblock && playsSkyblock -> MinecraftForge.EVENT_BUS.post(SkyblockJoinEvent())
-            playedSkyblock && !playsSkyblock -> MinecraftForge.EVENT_BUS.post(SkyblockLeaveEvent())
+        } finally {
+            skyblockCheckInProgress.set(false)
         }
     }
 }
