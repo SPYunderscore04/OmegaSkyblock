55b6cea40bacfd3fad4028d81ebf7dde412d48d2
Konstantin Lindorfer <konstantin.lindorfer@spyteam.one>
2023-08-05 23:18:08 +0200

    Added documentation for WorkerThread

--- src/main/kotlin/com/github/spyunderscore04/omegaskyblock/utils/WorkerThread.kt
+++ src/main/kotlin/com/github/spyunderscore04/omegaskyblock/utils/WorkerThread.kt
@@ -4,6 +4,17 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.asCoroutineDispatcher
 import java.util.concurrent.Executors
 
+/**
+ * Convenience object to always have a [CoroutineScope] ready for running things in parallel
+ *
+ * Especially useful to speed up event processing:
+ * ```
+ *     @SubscribeEvent
+ *     fun onEvent(event: Event) = WorkerThread.launch {
+ *         // do some heavy lifting
+ *     }
+ * ```
+ */
 object WorkerThread: CoroutineScope by CoroutineScope(
     Executors.newSingleThreadExecutor().asCoroutineDispatcher()
 )
